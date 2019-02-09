3bf99c7ee3d09f7a9272bb69d08b984a0aaba55c
Gaël Lhez <glhez@users.noreply.github.com>
2019-02-09 12:40:30 +0100

    fixed module-info for commons-{lang3,text}

--- java11/jpms-wrapper/org.apache.commons.text/src/main/java/module-info.java
+++ java11/jpms-wrapper/org.apache.commons.text/src/main/java/module-info.java
@@ -1,5 +1,10 @@
 module org.apache.commons.text {
   exports org.apache.commons.text;
+  exports org.apache.commons.text.diff;
+  exports org.apache.commons.text.lookup;
+  exports org.apache.commons.text.matcher;
+  exports org.apache.commons.text.similarity;
+  exports org.apache.commons.text.translate;
 
   requires java.base;
   // this was reported by jdeps, not sure it is mandatory
