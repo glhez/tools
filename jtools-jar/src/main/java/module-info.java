8987fea3d426016db6b890b05b61ca3f14ccd49a
Gaël LHEZ <glhez@users.noreply.github.com>
2023-03-01 01:07:38 +0100

    (fileset, pending)

--- jtools-jar/src/main/java/module-info.java
+++ jtools-jar/src/main/java/module-info.java
@@ -4,10 +4,11 @@
 module com.github.glhez.jtools.jar {
   exports com.github.glhez.jtools.jar;
 
+  requires com.github.glhez.fileset;
+  requires org.apache.logging.log4j;
   requires info.picocli;
-
   requires org.apache.commons.csv;
-  requires java.sql; // due to commons.csv
+  requires java.sql; // due to org.apache.commons.csv
 
   opens com.github.glhez.jtools.jar to info.picocli;
 
